94fb2c78da6c6cb4bd50bf2aa712b71ae5c0aa52
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-12 23:59:56 -0400

    restructured for react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Home from './pages/home/home'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          
+          <Route path="/" element={<Home/>}/>
+
+        </Routes>
+      </Router>
     </>
   )
 }
