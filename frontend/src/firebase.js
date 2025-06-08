0f784e55d7eef8165b14a7296427523672260893
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-06-08 14:46:59 -0400

    info hiding

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+const FIREBASE_APIKEY = import.meta.env.VITE_FIREBASE_APIKEY;
+const FIREBASE_AUTHDOMAIN = import.meta.env.VITE_FIREBASE_AUTHDOMAIN;
+const FIREBASE_PROJECTID = import.meta.env.VITE_FIREBASE_PROJECTID;
+const FIREBASE_STORAGEBUCKET = import.meta.env.VITE_FIREBASE_STORAGEBUCKET;
+const FIREBASE_MESSAGINGSENDERID = import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID;
+const FIREBASE_APPID = import.meta.env.VITE_FIREBASE_APPID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC0p6I_bUMt-cGOSQsYwTrSOiQU8JhXnts",
-  authDomain: "planaway-v0.firebaseapp.com",
-  projectId: "planaway-v0",
-  storageBucket: "planaway-v0.firebasestorage.app",
-  messagingSenderId: "155462035992",
-  appId: "1:155462035992:web:f5ab4d3ab6e5720b7a316d",
+  apiKey: FIREBASE_APIKEY,
+  authDomain: FIREBASE_AUTHDOMAIN,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+  appId: FIREBASE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
